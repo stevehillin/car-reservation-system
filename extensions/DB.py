6d58603992e53da85ac1767b515b8e62494c9c27
Steve Hillin <stevehillin@gmail.com>
2023-10-23 22:31:44 -0500

    update ExecUpdate method for prepared statement

--- extensions/DB.py
+++ extensions/DB.py
@@ -16,16 +16,40 @@ class DB():
         return mysql.connector.connect(**self.db_config)
     
     def ExecQuery(self, query):
+        retval = {'status': 'error', 'data': ''}
         try:
             conn = self.GetConnection()
             cursor = conn.cursor(dictionary=True)
             cursor.execute(query)
             result = cursor.fetchall()
+            if result:
+                retval['status'] = 'ok'
+                retval['data'] = result
             cursor.close()
             conn.close()
-            return result
+            
         except mysql.connector.Error as err:
-            app.logger.critical(str(err))
-            return str(err)
-        
+            app.logger.critical(f"Mysql Error: {str(err)}")
+            retval['data'] = str(err)
+        return retval
 
+    def ExecUpdate(self, query, data=False):
+        retval = {'status': 'error', 'data': ''}
+        try:
+            conn = self.GetConnection()
+            cursor = conn.cursor(prepared=True)
+            if data:
+                cursor.execute(query, data)
+            else:
+                cursor.execute(query)
+            conn.commit()
+            rowcount = cursor.rowcount
+            retval['status'] = 'ok'
+            retval['data'] = f'{rowcount} record(s) affected.'
+            cursor.close()
+            conn.close()
+            
+        except mysql.connector.Error as err:
+            app.logger.critical(f"Mysql Error: {str(err)}")
+            retval['data'] = str(err)
+        return retval
