fb334a9be7c4d8171f9dacdb2101538af6eed8db
Steve Hillin <stevehillin@gmail.com>
2023-10-23 19:19:03 -0500

    reorder endpoints to follow CRUD convention

--- modules/bookings/routes.py
+++ modules/bookings/routes.py
@@ -7,21 +7,20 @@ from app import app
 
 db = DB()
 
+@bp.route('/create', methods=['POST'])
+def create():
+    data = request.get_json()
+
 @bp.route('/', methods=['GET'])
 def read():
     # return jsonify({'message': 'Query executed successfully'})
     results = db.exec_query("SELECT * FROM bookings ORDER BY id DESC")
     return jsonify(results)
 
-@bp.route('/create', methods=['POST'])
-def create():
-    data = request.get_json()
-
+@bp.route('/update/<id>', methods=['POST'])
+def update(id):
+    pass
 
 @bp.route('/delete/<id>', methods=['POST'])
 def delete(id):
-    pass
-
-@bp.route('/update/<id>', methods=['POST'])
-def update(id):
     pass
--- modules/customers/routes.py
+++ modules/customers/routes.py
@@ -7,21 +7,19 @@ from app import app
 
 db = DB()
 
-@bp.route('/', methods=['GET'])
-def read():
-    # return jsonify({'message': 'Query executed successfully'})
-    results = db.exec_query("SELECT * FROM customers ORDER BY id DESC")
-    return jsonify(results)
-
 @bp.route('/create', methods=['POST'])
 def create():
     data = request.get_json()
 
-
-@bp.route('/delete/<id>', methods=['POST'])
-def delete(id):
-    pass
+@bp.route('/', methods=['GET'])
+def read():
+    results = db.exec_query("SELECT * FROM customers ORDER BY id DESC")
+    return jsonify(results)
 
 @bp.route('/update/<id>', methods=['POST'])
 def update(id):
+    pass
+
+@bp.route('/delete/<id>', methods=['POST'])
+def delete(id):
     pass
